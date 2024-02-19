16222af47e1b50ea7e8d53fbda02817ec131a6a5
sabrineayadi1 <ayadisabrine@tbs.u-tunis.tn>
2024-02-19 14:18:58 +0100

    w4 React Multiple Components

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,7 @@
-/* import * as React from "react";
-function App(){
+ import * as React from "react";
+ export default App; 
+ /*week1*/
+/*function App(){
   const title = "React";
   return(
     <div>
@@ -8,8 +10,8 @@ function App(){
       <input id="search" type="text" />
     </div>
   );
-}
-export default App; */
+}*/
+
 /* import * as React from "react";
 const welcome = {
   greeting:"Hey",
@@ -28,7 +30,7 @@ function App(){
   );
 }
 export default App; */
-import * as React from "react";
+/* import * as React from "react";
 function getTitle(title){
   return title;
 }
@@ -41,4 +43,62 @@ function App(){
     </div>
   );
 }
-export default App;
+export default App; */
+/*week2*/
+const list = [
+  {
+    title:"React",
+    url:"https://reactjs.org/",
+    author:"Jordan Walke",
+    num_comments: 3 ,
+    points : 4,
+    objectID : 0,
+  },
+  {
+    title:"Redux",
+    url:"https://redux.js.org/",
+    author:"Dan Abramov , Andrew Clark",
+    num_comments: 2,
+    points : 5,
+    objectID : 1,
+  }
+]
+function List(){
+  return(
+    <div>
+      <ul>
+        {list.map(function(item){
+          return(
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title} </a>
+              </span>
+              <span>{item.author} </span>
+              <span> {item.num_comments} , </span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+function Search(){
+  return(
+    <div>
+<label htmlFor="search">Search:</label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+function App(){
+  return(
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search />
+      <hr/>
+      <List />
+    
+    </div>
+  );
+}
