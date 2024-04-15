95b7c7fa52abdc206f8fb6a09ed5d2daf0d7b67e
sabrineayadi1 <ayadisabrine@tbs.u-tunis.tn>
2024-04-15 14:08:43 +0100

    W6 Adding React props

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,5 @@
  import * as React from "react";
- export default App; 
+
  /*week1*/
 /*function App(){
   const title = "React";
@@ -45,7 +45,7 @@ function App(){
 }
 export default App; */
 /*week2*/
-const list = [
+/* const list = [
   {
     title:"React",
     url:"https://reactjs.org/",
@@ -61,9 +61,9 @@ const list = [
     num_comments: 2,
     points : 5,
     objectID : 1,
-  }
-]
-function List(){
+  } 
+] */
+/* function List () {
   return(
     <div>
       <ul>
@@ -82,23 +82,79 @@ function List(){
       </ul>
     </div>
   );
-}
-function Search(){
+} */
+/* function Search (){
   return(
     <div>
 <label htmlFor="search">Search:</label>
       <input id="search" type="text" />
     </div>
   );
-}
-function App(){
-  return(
+} */
+
+
+const List = (props) =>(
+    
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item}/>
+    ))}
+    </ul>
+);
+const Item = (props) => (
+     <li>
+        
+          <span>
+            <a href={props.item.url}>{props.item.title} </a>
+          </span>
+          <span>{props.item.author} </span>
+          <span> {props.item.num_comments} , </span>
+          <span>{props.item.points}</span>
+        </li>
+);
+
+const Search = () => {
+  const handleChange = (event) =>{
+    console.log(event);
+    console.log(event.target.value);
+  };
+   return( <div>
+<label htmlFor="search">Search : </label>
+      <input id="search" type="text" onChange={handleChange} />
+    </div>
+  );
+   };
+
+
+const App = () =>{
+  const stories = [
+    {
+      title:"React",
+      url:"https://reactjs.org/",
+      author:"Jordan Walke",
+      num_comments: 3 ,
+      points : 4,
+      objectID : 0,
+    },
+    {
+      title:"Redux",
+      url:"https://redux.js.org/",
+      author:"Dan Abramov , Andrew Clark",
+      num_comments: 2,
+      points : 5,
+      objectID : 1,
+    } ,
+  ];
+
+
+return (
     <div>
       <h1>My Hacker Stories</h1>
       <Search />
       <hr/>
-      <List />
+      <List list={stories} />
     
     </div>
   );
-}
+};
+export default App;
